3d85d7d1515f5df080b5324f2d97cc365ca61382
Thor Muto Asmund <thorasmund@gmail.com>
2021-05-01 00:48:19 +0200

    * First commit

---
+++ MasterMind/MasterMindEngine.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind
+{
+    public class MasterMindEngine
+    {
+        private List<string> pool;
+        private string letters = "WBRGUY";
+
+        public void Reset()
+        {
+            this.pool = new List<string>();
+            foreach (var a in this.letters.ToArray())
+            {
+                var option1 = a.ToString();
+                foreach (var b in this.letters.ToArray().Where(q => q != a))
+                {
+                    var option2 = option1 + b.ToString();
+                    foreach (var c in this.letters.ToArray().Where(q => q != a && q != b))
+                    {
+                        var option3 = option2 + c.ToString();
+                        foreach (var d in this.letters.ToArray().Where(q => q != a && q != b && q != c))
+                        {
+                            this.pool.Add(option3 + d.ToString());
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(this.pool.Count);
+        }
+
+        public string Guess()
+        {
+            return this.pool[new Random().Next(0, this.pool.Count)];
+        }
+
+        public bool Answer(string answer, string myGuess, out bool finished)
+        {
+            finished = false;
+            answer.Replace(" ", "");
+            var test = answer.Replace("W", "").Replace("B", "");
+            if (test.Length > 0 || answer.Length < 2 || answer.Length > 4)
+            {
+                return false;
+            }
+
+            if (answer == "BBBB")
+            {
+                finished = true;
+                return true;
+            }
+
+            var bAnswer = answer.Replace("W", "");
+            var wAnswer = answer.Replace("B", "");
+            RemoveGuessesWithNotXCorrect(myGuess, bAnswer.Length, wAnswer.Length);
+            Console.WriteLine($"Pool size {this.pool.Count}");
+
+            if (this.pool.Count == 0)
+            {
+                finished = true;
+                return false;
+            }
+
+            return true;
+        }
+
+        private void RemoveGuessesWithNotXCorrect(string guess, int b, int w)
+        {
+            foreach (var option in this.pool.ToArray())
+            {
+                var c = (option[0] == guess[0] ? 1 : 0) + (option[1] == guess[1] ? 1 : 0) +
+                    (option[2] == guess[2] ? 1 : 0) + (option[3] == guess[3] ? 1 : 0);
+                if (c != b)
+                {
+                    pool.Remove(option);
+                }
+            }
+            foreach (var option in this.pool.ToArray())
+            {
+                var c = (option.Contains(guess[0]) && option[0] != guess[0] ? 1 : 0) + (option.Contains(guess[1]) && option[1] != guess[1] ? 1 : 0) +
+                    (option.Contains(guess[2]) && option[2] != guess[2] ? 1 : 0) + (option.Contains(guess[3]) && option[3] != guess[3] ? 1 : 0);
+                if (c != w)
+                {
+                    pool.Remove(option);
+                }
+            }
+        }
+    }
+}
---
+++ MasterMind/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mind = new MasterMindEngine();
+
+            Console.WriteLine("Welcome to the MasterMind bot!");
+            Console.WriteLine("Write down on a piece of paper your secret code consisting of four colors.");
+            Console.WriteLine("The colors are White, B (Black), R (Red), G (Green), U (Blue) and Y (Yellow). Each color must be used only once.");
+            Console.WriteLine("A legal code could be 'BRGW'.");
+            Console.WriteLine("Whenever I make a guess you have to answer with 2-4 letters:");
+            Console.WriteLine("  write B (black) if one of my guesses is at the correct place.)");
+            Console.WriteLine("  write W (white) if the pin is correct but in a wrong place.");
+            Console.WriteLine("So an example of an answer could be BBW  or it could be WWWW or even just WW.");
+
+            string q;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Press enter to start a new game or type Q to quit. (you can type Q at any time to quit the game)");
+                q = Console.ReadLine().ToUpper().Trim();
+                if (q == "Q")
+                {
+                    break;
+                }
+
+                var cnt = 0;
+                bool finished = false;
+                bool isLegal;
+                mind.Reset();
+                Console.WriteLine("================");
+
+                do
+                {
+                    cnt++;
+                    var guess = mind.Guess();
+                    isLegal = false;
+
+                    while (!isLegal)
+                    {
+                        Console.WriteLine($"My {cnt}. guess is {guess}. What is your answer?");
+                        q = Console.ReadLine().ToUpper();
+                        if (q == "Q")
+                        {
+                            break;
+                        }
+
+                        isLegal = mind.Answer(q, guess, out finished);
+                        if (!isLegal)
+                        {
+                            if (!finished)
+                            {
+                                Console.WriteLine($"That is not a legal answer.");
+                                Console.WriteLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine($"I don't know what to guess... I have no clue what you said. I think you might have made an incorrect answer at some point. Please start a new game.");
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+                while (q != "Q" && !finished);
+
+                if (finished && isLegal)
+                {
+                    Console.WriteLine($"Cool! I managed to guess your code in only {cnt} attempts!");
+                    Console.WriteLine();
+                }
+            }
+            while (q != "Q");
+
+            Console.WriteLine("Ok, bye bye");
+        }
+    }
+}
